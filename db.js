1910ed4a02d695b1168fcf0b01b8bbdc9ae503a7
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-03-10 13:59:36 +0400

    change user's schema by adding appropriate field type to every field

--- db.js
+++ db.js
@@ -4,7 +4,12 @@ const userData = mongoose.Schema({
   username: String,
   
   count: {type:Number, default:0},
-  logs: Array
+  log: [{
+    description: String,
+    duration: Number,
+    date: String
+}
+  ]
 })
 
 module.exports = mongoose.model('user', userData);
